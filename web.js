1a2a50edc2f6beb2b15187839b6a8ec1c8b2b6ad
Chris Jenkins <cdpjenkins@gmail.com>
2013-07-14 12:06:09 +0100

    Call write not send.

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
     var buf = fs.readFileSync('index.html');
-    response.send(buf)
+    response.write(buf)
 //  response.send('Hello World 2!');
 });
 
